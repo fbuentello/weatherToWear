961b658db65c09df8a0db780e78ad64246e9e4d8
Fabian Buentello <fbuentello@mioty.com>
2015-02-27 06:47:04 -0600

    Initial Commit

---
+++ What to Wear/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  What to Wear
+//
+//  Created by Fabian Buentello on 2/27/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ What to Wear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  What to Wear
+//
+//  Created by Fabian Buentello on 2/27/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
